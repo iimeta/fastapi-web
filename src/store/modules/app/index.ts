d87f2fca86c0a397883e16c5d26c9f975681af83
Loka <loka@iim.ai>
2025-02-20 16:48:28 +0800

    更新版本号

--- src/store/modules/app/index.ts
+++ src/store/modules/app/index.ts
@@ -123,6 +123,11 @@ const useAppStore = defineStore('app', {
       return (
         state.config.announcements || [
           {
+            content: 'v1.3.0 版本发布啦! 啦!! 啦!!!',
+            jump_url:
+              'https://github.com/iimeta/fastapi-admin/releases/tag/v1.3.0',
+          },
+          {
             content: 'v1.2.0 版本发布啦! 啦!! 啦!!!',
             jump_url:
               'https://github.com/iimeta/fastapi-admin/releases/tag/v1.2.0',
@@ -131,11 +136,6 @@ const useAppStore = defineStore('app', {
             content: 'v1.1.0 版本发布啦! 啦!! 啦!!!',
             jump_url:
               'https://github.com/iimeta/fastapi-admin/releases/tag/v1.1.0',
-          },
-          {
-            content: 'v1.0.0 版本发布啦! 啦!! 啦!!!',
-            jump_url:
-              'https://github.com/iimeta/fastapi-admin/releases/tag/v1.0.0',
           },
         ]
       );
