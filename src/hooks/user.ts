20f2a3f3916fe936f61c0a5f272d05a461651c7a
Loka <loka@iim.ai>
2026-01-11 14:49:26 +0800

    完善国际化

--- src/hooks/user.ts
+++ src/hooks/user.ts
@@ -1,16 +1,18 @@
+import { useI18n } from 'vue-i18n';
 import { useRouter } from 'vue-router';
 import { Message } from '@arco-design/web-vue';
 
 import { useUserStore } from '@/store';
 
 export default function useUser() {
+  const { t } = useI18n();
   const router = useRouter();
   const userStore = useUserStore();
   const logout = async (logoutTo?: string) => {
     const userRole = localStorage.getItem('userRole');
     await userStore.logout();
     const currentRoute = router.currentRoute.value;
-    Message.success('退出成功');
+    Message.success(t('login.logout.success'));
 
     let routeName =
       logoutTo && typeof logoutTo === 'string' ? logoutTo : 'login';
